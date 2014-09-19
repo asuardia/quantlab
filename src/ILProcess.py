857ab8fb235fc360c41317d495e9f9e1f1e375b5
asuardia <antonio.suardiaz@gmail.com>
2014-09-20 00:03:54 +0200

    Python

--- src/ILProcess.py
+++ src/ILProcess.py
@@ -8,6 +8,16 @@ def transform(data):
         ndict[nkey] = transform(value) if type(dict())==type(value) else value
     return ndict
 
+def process(action, fileIn, fileOut):
+    fhdl,pathIn= tempfile.mkstemp()
+    il = eval (open(fileIn).read ())
+    f = os.fdopen(fhdl, 'w')
+    f.write(json.dumps(transform (il)))
+    f.close()
+    os.system("runhaskell C:\Projects\quantlab\src\Interfaces\\" + action + ".hs " + pathIn + " " + fileOut)
+    os.remove(pathIn)
+
+'''  
 fhdl,pathIn= tempfile.mkstemp()
 il = eval (open(sys.argv[2]).read ())
 f = os.fdopen(fhdl, 'w')
@@ -16,6 +26,5 @@ f.close()
 action = sys.argv[1]
 fileOut = sys.argv[3]
 os.system("C:\\Projects\\quantlab\\src\\Interfaces\\" + action + ".exe " + pathIn + " " + fileOut)
-os.remove(pathIn)
-
-
+os.remove(pathIn)    
+'''
